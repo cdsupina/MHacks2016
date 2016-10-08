cd956176fae079347aab600b0000413ca01f594c
Carlo Supina <cdsupina@gmail.com>
2016-10-08 18:29:33 -0400

    twitch commands completely working

--- Twitch/mvmt_val.py
+++ Twitch/mvmt_val.py
@@ -6,24 +6,36 @@ Created on Sat Oct  8 12:24:53 2016
 @author: Varoon
 """
 
-#takes array. Returns -1 if sum of vals is nonpositive. Else returns the index of the max val.            
-def mvmt_val_helper(arr):
+#takes array. Returns -1 if sum of vals is nonpositive. Else returns the index of the max val.
+def movement_helper(arr):
     if sum(arr)<=0:
         return -1
     else:
         return arr.index(max(arr))
 
-# returns 0 if no inputs from twitch. Otherwise outputs distinct value for each cardinal and intercardinal 
+# returns 0 if no inputs from twitch. Otherwise outputs distinct value for each cardinal and intercardinal
 # direction. All values between 0 and 10, inclusive. Powers of two guarantee distinctiveness under addition
-        
-def get_mvmt_val(commands,team):    
-    if sum(commands[team][0:2])==0 or sum(commands[team])==0:     #if no forward or backward movement, side movement is irrelevent
+
+def democracy_val(commands_list,team):
+    if sum(commands_list[team][0:2])==0 or sum(commands_list[team])==0:     #if no forward or backward movement, side movement is irrelevent
         return 0
-    
-    #beyond this, assuming forward/backward movement.     
-    forward_back_val=mvmt_val_helper(commands[team][0:2])
-    left_right_val=mvmt_val_helper(commands[team][2:4])
+
+    #beyond this, assuming forward/backward movement.
+    forward_back_val=movement_helper(commands_list[team][0:2])
+    left_right_val=movement_helper(commands_list[team][2:4])
     if left_right_val == -1:
         return 2**(forward_back_val)
     return 2**(forward_back_val) + 2**(left_right_val+2)
-    
+
+def anarchy_val(commands_list,team):
+    if sum(commands_list[team])==0:
+        return 0
+
+    #beyond this, assuming movement
+    forward_back_val=movement_helper(commands_list[team][0:2])
+    left_right_val=movement_helper(commands_list[team][2:4])
+    if left_right_val == -1:
+        return 2**(forward_back_val)
+    if forward_back_val == -1:
+        return 2**(2+left_right_val)
+    return 2**(forward_back_val) + 2**(left_right_val+2)
---
+++ Twitch/rc_car_anarchy.py
@@ -0,0 +1,102 @@
+#@author: Varoon
+#This script is meant to listen to the twitch stream from hot_glue_from_purdue, interact with the
+#players in the stream, and send the "consensus" of the players to the console. Mainly for testing
+# This will also assign and maintain teams.
+import socket
+import time
+import re
+import random
+import mvmt_val
+import serial
+import struct
+
+#gives the introductory messages to the users.
+def introduction_messages():
+    s.send('PRIVMSG %s :Welcome to the game!\r\n' %(CHAN))
+    time.sleep(2)
+    s.send('PRIVMSG %s :The rules are very simple. You will be assigned to a team that controls a car.\r\n' %(CHAN))
+    time.sleep(2)
+    s.send('PRIVMSG %s :Type f,b,l, or r, to go forward, backward, left, and right, respectively.\r\n' %(CHAN))
+    time.sleep(2)
+    s.send('PRIVMSG %s :The collective decisions of your team will decide your car\'s movement.\r\n' %(CHAN))
+    time.sleep(2)
+    s.send('PRIVMSG %s :The team whose car leaves the ring loses. Good Luck!\r\n' %(CHAN))
+
+HOST = "irc.twitch.tv"
+PORT = 6667
+
+#FOR SIMPLICITY, 0=RED_TEAM, 1=BLUE TEAM
+teams={0:'Red',1:'Blue'}
+#From the moderator channel
+NICK = "twitchplaysbattlebots"
+PASS = "oauth:0dyincbnyg1swo4y4eirxn6iczixdo"
+
+#From the streamer channel
+CHAN = "#twitchplaysbattlebots"
+
+ser = serial.Serial('/dev/ttyACM0',9600)
+
+#viewers need to register for one of two teams
+registered_users = {}
+
+numRed=0;
+numBlue=0
+#list of lists for commands. Red team then blue team. Forward, back, left, right
+commands_list=[[0,0,0,0],[0,0,0,0]]
+command_lookup={'f':0, 'b':1,'l':2,'r':3}
+CHAT_MSG = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
+
+s = socket.socket()
+s.connect((HOST,PORT))
+s.send("PASS {}\r\n".format(PASS).encode("utf-8"))
+s.send("NICK {}\r\n".format(NICK).encode("utf-8"))
+s.send("JOIN {}\r\n".format(CHAN).encode("utf-8"))
+
+while True:
+
+        line = s.recv(1024).decode("utf-8")
+        if line == "PING :tmi.twitch.tv\r\n":
+            s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+        else:
+            username = re.search(r"\w+",line).group(0)
+
+            # if user is not registered. All cases handle bug where "tmi" (name of twitch irc server) registers as player
+            if not registered_users.has_key(username) and username!="tmi" and username!=NICK:
+                if numRed<numBlue:
+                    registered_users[username]=0
+                    numRed+=1
+                    s.send('PRIVMSG %s :%s\r\n' % (CHAN, '{} is now on the {} Team'.format(username, teams.get(0).encode('utf-8'))))
+                elif numBlue<numRed:
+                    registered_users[username]=1
+                    numBlue+=1
+                    s.send('PRIVMSG %s :%s\r\n' % (CHAN, '{} is now on the {} Team'.format(username, teams.get(1).encode('utf-8'))))
+
+                else:
+
+                    if random.randint(0,1)==0:
+                        registered_users[username]=0
+                        numRed+=1
+                        s.send('PRIVMSG %s :%s\r\n' % (CHAN, '{} is now on the {} Team'.format(username, teams.get(0).encode('utf-8'))))
+
+                    else:
+                        registered_users[username]=1
+                        numBlue+=1
+                        s.send('PRIVMSG %s :%s\r\n' % (CHAN, '{} is now on the {} Team'.format(username, teams.get(1).encode('utf-8'))))
+
+            else: #if the user is registered
+
+                if registered_users.has_key(username):
+                    message = CHAT_MSG.sub("",line).rstrip()
+
+                    #increments commands list if possible
+
+                    if command_lookup.has_key(message):
+                        commands_list[registered_users.get(username)][command_lookup.get(message)]+=1
+                        print commands_list
+                        if registered_users.get(username)==0:
+                            ser.write(struct.pack('>BB', mvmt_val.anarchy_val(commands_list,0),0))
+                        else:
+                            ser.write(struct.pack('>BB', 0, mvmt_val.anarchy_val(commands_list,1)))
+                        print('team: '+str(registered_users.get(username)))
+                        print('command: '+ str(mvmt_val.anarchy_val(commands_list,registered_users.get(username))))
+                        commands_list=[[0,0,0,0],[0,0,0,0]]
